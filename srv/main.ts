65b720f12e4a9be20eb691a9092083b57d9d20ac
Pedro Victor <pedrovicf@outlook.com>
2025-10-20 17:50:07 -0300

    feat: add libraries cds-type

--- srv/main.ts
+++ srv/main.ts
@@ -1,3 +1,14 @@
+import { Customer, Customers } from '@models/sales';
+
+const customer: Customer = {
+    email: 'pedro@numen.com',
+    firstName: 'Pedro',
+    lastName: 'Victor',
+    id: '1234'
+};
+
+const customers: Customer[] = [customer] 
+
 const funcao = (variavel: string) => console.log(variavel);
 
 funcao('123');
