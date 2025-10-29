a566d8ef21f445c18c77a9d67b5a23fc09154ad1
Pedro Victor <pedrovicf@outlook.com>
2025-10-29 17:35:12 -0300

    feat: make use of refactored logic in cap service

--- srv/main.ts
+++ srv/main.ts
@@ -34,32 +34,8 @@ export default (service: Service) => {
         request.data.totalAmount = result.totalAmount;
         
     });
-    service.after('CREATE', 'SalesOrderHeaders', async (results: SalesOrderHeaders, request: Request) => {
-        const headersAsArray = Array.isArray(results) ? results : [results] as SalesOrderHeaders;
-
-        for (const header of headersAsArray) {
-            const items = header.items as SalesOrdersItems;
-            const productsData = items.map(item => ({
-                id: item.product_id as string,
-                quantity: item.quantity as number
-            }));
-            const productsIds: string[] = productsData.map((productData) => productData.id);
-            const productsQuery = SELECT.from('sales.Products').where({ id: productsIds});
-            const products: Products = await cds.run(productsQuery); 
-            for (const productData of productsData) {
-                const foundProduct = products.find(product => product.id === productData.id) as Product;
-                foundProduct.stock = (foundProduct.stock as number) - productData.quantity;
-                await cds.update('sales.Products').where({ id: foundProduct.id }).with({ stock: foundProduct.stock });
-            }
-            const headersAsString = JSON.stringify(header);
-            const userAsString = JSON.stringify(request.user);
-            const log = [{
-                header_id: header.id,
-                userData: userAsString,
-                orderData: headersAsString
-            }];
-               await cds.create('sales.SalesOrderLogs').entries(log)
-        }
+    service.after('CREATE', 'SalesOrderHeaders', async (SalesOrderHeaders: SalesOrderHeaders, request: Request) => {
+        await salesOrderHeaderController.afterCreate(SalesOrderHeaders, request.user);
    
         
      
