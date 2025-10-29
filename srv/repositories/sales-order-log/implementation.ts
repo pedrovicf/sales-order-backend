5f4e040f19aa233202527b02955525904b8e6d03
Pedro Victor <pedrovicf@outlook.com>
2025-10-29 17:31:28 -0300

    feat: add SalesOrderLogRepositoryImpl

---
+++ srv/repositories/sales-order-log/implementation.ts
@@ -0,0 +1,10 @@
+import { SalesOrderLogModel } from "srv/models/sales-order-log";
+import { SalesOrderLogRepository } from "./protocols";
+import cds from "@sap/cds";
+
+export class SalesOrderLogRepositoryImpl implements SalesOrderLogRepository {
+    public async create(logs: SalesOrderLogModel[]): Promise<void> {
+        const logsObjects = logs.map(log => log.toObject());
+        await cds.create('sales.SalesOrderLogs').entries(logsObjects);
+    }
+}
