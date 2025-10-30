2b5975164752311f53661d7432197bf0859488cc
Pedro Victor <pedrovicf@outlook.com>
2025-10-30 16:12:38 -0300

    feat: add eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,2 +1,50 @@
-import cds from '@sap/cds/eslint.config.mjs'
-export default [ ...cds.recommended ]
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+    {
+        languageOptions: {
+            globals: globals.node
+        }
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        ignores: ['./gen/*.{js,ts}'],
+        files: ['**/*.{mjs,js,ts}'],
+        rules: {
+            // Regra principal (foi corrigido o alinhamento das opções)
+            '@typescript-eslint/no-unused-vars' : [
+                'error',
+                {
+                    caughtErrors: 'all',
+                    caughtErrorsIgnorePattern: '^ignore',
+                    ignoreRestSiblings: true
+                }
+            ],
+            // Regras de estilo (indentação corrigida)
+            'eol-last': 'error',
+            indent: [
+                'error',
+                4,
+                {
+                    SwitchCase: 1
+                }
+            ],
+            'max-len': ['error', 120],
+            'max-lines-per-function': ['error', 30],
+            'object-curly-spacing': ['error', 'always'],
+            quotes: ['error', 'single'],
+            'quote-props': ['error', 'as-needed'],
+            semi: ['error', 'always'],
+            'sort-imports': [
+                'error',
+                {
+                    memberSyntaxSortOrder: ['single', 'all', 'multiple','none'],
+                    allowSeparatedGroups: true
+                }
+            ]
+        }
+    }
+];
