9eaaa4ea6ffed4f04df9d9c590f7eae34642afd0
Pedro Victor <pedrovicf@outlook.com>
2025-11-13 16:19:15 -0300

    feat: add  salesorderheaderrepository

--- srv/factories/services/sales-order-header.ts
+++ srv/factories/services/sales-order-header.ts
@@ -1,14 +1,21 @@
 import { CustomerRepositoryImpl } from '@/repositories/customer/implementation';
 import { ProductRepositoryImpl } from '@/repositories/product/implementation';
+import { SalesOrderHeaderRepositoryImpl } from '@/repositories/sales-order-header/implementation';
 import { SalesOrderHeaderService } from '@/services/sales-order-header/protocols';
 import { SalesOrderHeaderServiceImpl } from '@/services/sales-order-header/implementation';
 import { SalesOrderLogRepositoryImpl } from '@/repositories/sales-order-log/implementation';
 
 const makeSalesOrderHeaderService = (): SalesOrderHeaderService => {
+    const salesOrderHeaderRepository = new SalesOrderHeaderRepositoryImpl();
     const customerRepository = new CustomerRepositoryImpl();
     const productRepository = new ProductRepositoryImpl();
     const salesOrderLogRepository = new SalesOrderLogRepositoryImpl();
-    return new SalesOrderHeaderServiceImpl(customerRepository, productRepository, salesOrderLogRepository);
+    return new SalesOrderHeaderServiceImpl(
+        salesOrderHeaderRepository,
+        customerRepository,
+        productRepository,
+        salesOrderLogRepository
+    );
 };
 
 export const salesOrderHeaderService = makeSalesOrderHeaderService();
